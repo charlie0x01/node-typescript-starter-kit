c2a2d997da68ec130c3f5fb131377098c5084e40
Abdullah Tanveer <abdullahtanveer56@outlook.com>
2024-08-15 19:09:48 +0500

    config(swagger): configure to swagger to auto generate api documentation
    swagger shall auto generate documention for each api endpoint with given comments. it'll build auto generate docs on every change and then restart the server
    
    Swagger Configured using swagger-autogen and swagger-ui-express

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,32 @@
 import express, { Application, Request, Response } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
+
+import swaggerDocument from './swagger/swaggerDocumentation.json';
 
 const app: Application = express();
 
 // body parser
 app.use(express.json());
+// use cors
+app.use(cors());
+// set public folder for static files
+app.use(express.static(__dirname?.replace('src', '') + 'public'));
+// swagger document route
+// when you hit this route you shall auto generated swagger documentation
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // simple endpoint
-app.get('/', (req: Request, res: Response) => {
+
+app.get('/api/v1', (req: Request, res: Response) => {
+  // #swagger.summary = "Welcome API Endpoint to start with multiline"
+  /*  #swagger.responses[200] = {
+            description: 'Some description...',
+            schema: {
+                sucess: true,
+                message: "Welcome to Node Typescript Boilerplate"
+            }
+    } */
   return res.status(200).json({ success: true, message: 'Welcome to Node Typescript Boilerplate' });
 });
 
---
+++ src/swagger/swagger.ts
@@ -0,0 +1,36 @@
+import swaggerAutogen from 'swagger-autogen';
+
+import { boldText } from '../utils/consoleColors.ts';
+
+const documentConfiguration = {
+  info: {
+    version: '1.0.0', // by default: '1.0.0'
+    title: 'NodeJS Typescript Boilerplate', // by default: 'REST API'
+    description: 'A Starter Kit for NodeJS REST APIs', // by default: ''
+  },
+  servers: [
+    {
+      url: 'http://localhost:8000', // by default: 'http://localhost:3000'
+      description: '', // by default: ''
+    },
+    // { ... }
+  ],
+  tags: [
+    // by default: empty Array
+    {
+      name: '', // Tag name
+      description: '', // Tag description
+    },
+    // { ... }
+  ],
+  components: {}, // by default: empty object
+};
+
+export const outputFile = './src/swagger/swaggerDocumentation.json';
+export const routes = ['../app.ts'];
+swaggerAutogen({ openapi: '3.0.0' })(outputFile, routes, documentConfiguration)
+  .then(async () => {
+    // run server when swagger done with documention
+    return await import('../index.ts');
+  })
+  .catch((error) => console.log(boldText.RED, error));
