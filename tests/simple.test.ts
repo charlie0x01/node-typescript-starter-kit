688d6aa9117b1f712c939e49f68658c2cc1b7c7f
Abdullah Tanveer <abdullahtanveer56@outlook.com>
2024-08-20 16:46:19 +0500

    feat: add a test for welcome api
    Test to test jest configuration

--- tests/simple.test.ts
+++ tests/simple.test.ts
@@ -1,14 +1,15 @@
-describe('Testing Jest', () => {
-  beforeAll(() => {
-    console.log('Before All!');
-  });
+import request from 'supertest';
 
-  afterAll(() => {
-    console.log('After All!');
-  });
-
-  test('Sample Test', () => {
-    const num = 2 * 2;
-    expect(num).toBe(4);
+import app from '../src/app';
+describe('Simple', () => {
+  test('Welcome APIs Test', async () => {
+    // define number of assertions expected to be true
+    expect.assertions(2);
+    // test request
+    const response = await request(app).get('/api/v1');
+    // assertion 1, test is response success is equel to true
+    expect(response?.body?.success).toBe(true);
+    // assertion 2, test response message is equel to expected message
+    expect(response?.body?.message).toBe('Welcome to Node Typescript Starter Kit');
   });
 });
